a0bad98cb84bb6ac9fd395e23dec8dc3e48c5c1f
soulaiman-bo <bouhlal.soulaiman@gmail.com>
2023-12-16 22:11:39 +0100

    I changed the queryStatment from private into static. and i Created WhereOperators class it handles the integration of operators after Where clause.

--- src/SQL.php
+++ src/SQL.php
@@ -1,12 +1,63 @@
 <?php
 
 
+class WhereOperators extends SQL {
+
+    public function __construct(){
+    }
+
+    //$validOperators = ['=', '<', '>', '<=', '>=', '!=', 'LIKE'];
+
+
+    public function equals($field){
+        SQL::$queryStatment .= " = " .  $field;
+        return $this;
+    }
+
+    public function notEqual($field){
+        SQL::$queryStatment .= " != " .  $field;
+        return $this;
+    }
+
+    public function lessThan($field){
+        SQL::$queryStatment .= " < " .  $field;
+        return $this;
+    }
+
+    public function moreOrEqual($field){
+        SQL::$queryStatment .= " > " .  $field;
+        return $this;
+    }
+
+    public function lessOrEqual($field){
+        SQL::$queryStatment .= " <= " .  $field;
+        return $this;
+    }
+
+    public function moreThan($field){
+        SQL::$queryStatment .= " >= " .  $field;
+        return $this;
+    }
+
+    public function between($field1, $field2){
+        SQL::$queryStatment .= " BETWEEN " .  $field1 .  " AND " . $field2;
+        return $this;
+    }
+
+    public function like($field){
+        SQL::$queryStatment .= " LIKE " .  $field;
+        return $this;
+    }
+
+}
+
+
 class SQL
 {
 
     private $connection;
     private $tableName;
-    private $queryStatment;
+    protected static  $queryStatment;
     private $currentStatmentType;
 
     public function __construct(string $connection)
@@ -15,17 +66,19 @@ class SQL
         return $this;
     }
 
+    
+
     public function select($ArrayOfFields = ["*"])
     {
 
         $this->currentStatmentType = "select";
-        $this->queryStatment = "SELECT ";
-        
+        SQL::$queryStatment = "SELECT ";
+
         for ($i = 0; $i < count($ArrayOfFields); $i++) {
-            $this->queryStatment .= $ArrayOfFields[$i] . ", ";
+            SQL::$queryStatment .= $ArrayOfFields[$i] . ", ";
         }
 
-        $this->queryStatment = rtrim($this->queryStatment, ', ');
+        SQL::$queryStatment = rtrim(SQL::$queryStatment, ', ');
 
         return $this;
     }
@@ -34,29 +87,39 @@ class SQL
     {
         $this->tableName = $tableName;
 
-        $this->queryStatment .=  " FROM " . $this->tableName;
+        SQL::$queryStatment .=  " FROM " . $this->tableName;
 
         return $this;
     }
 
-    public function where(...$ArrayOfFields)
+    public function where(string $field)
     {
+        $operators = new WhereOperators();
+
+        SQL::$queryStatment .= " WHERE " . $field;
+
+        return $operators;
+
     }
 
     public function save()
     {
-        // $query = $this->connection->prepare($this->queryStatment);
+        // $query = $this->connection->prepare(SQL::$queryStatment);
         // $query->execute();
         // $result = $query->get_result();
         // $query->close();
         // return $result;
 
-        echo $this->queryStatment;
+        echo SQL::$queryStatment . ";";
     }
 
 }
 
 
+
+
 $sql = new SQL("connection");
 
-$sql->select()->from("users")->save();
+$sql->select()->from("users")->where("ID")->between(15, 50)->save();
+
+
